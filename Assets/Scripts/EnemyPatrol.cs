7c359f7118c663bcaf42b17980865c3a0146a1c2
shafirasalim <shafira03excellent@gmail.com>
2026-04-19 23:00:24 +0700

    EnemyPatrol dan PlayerMovements

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed = 2f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool movingRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movingRight ? speed : -speed, rb.velocity.y);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, 1f, groundLayer);
+
+        Debug.Log(movingRight);
+        Debug.DrawRay(groundCheck.position, Vector2.down * 1f, Color.red);
+
+        if (groundInfo.collider == null)
+        {
+            Flip();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Kalau nabrak tembok / object → balik arah
+        if (collision.collider.CompareTag("Ground"))
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        movingRight = !movingRight;
+
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    void Update()
+    {
+
+
+        Debug.DrawRay(groundCheck.position, Vector2.down * 1f, Color.red);
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && IsGrounded ())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+        if (Input.GetButtonUp ("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+
+    [SerializeField] private Collider2D groundCheckCollider;
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);
+    }
+
+    private void Flip()
+    {
+       if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, 0.6f);
+        }
+    }
+
+}
